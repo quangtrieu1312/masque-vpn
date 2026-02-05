a9ed832242736e7968d6df5fcfeeb926c7207627
Trieu Truong <quangtrieu1312@gmail.com>
2026-02-05 04:45:21 -0500

    Small fix

--- server/scripts/bootstrap/main.sh
+++ server/scripts/bootstrap/main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$(realpath $(dirname $0))
 ls "$SCRIPT_DIR" | grep -E '^[0-9]+_.*' | sort | while read script; do
-    chmod +x "$script"
-    bash -c "$script"
+    chmod +x "$SCRIPT_DIR/$script"
+    bash -c "$SCRIPT_DIR/$script"
 done
