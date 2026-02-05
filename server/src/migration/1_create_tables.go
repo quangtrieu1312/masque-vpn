abccfce3e0d4baee8b689a722c8866f71e7cf373
Trieu Truong <quangtrieu1312@gmail.com>
2026-02-05 05:35:52 -0500

    Small fix

--- server/src/migration/1_create_tables.go
+++ server/src/migration/1_create_tables.go
@@ -27,57 +27,57 @@ func (m Migration1) Run() int {
         logger.Fatal(fmt.Sprintf("cannot start DB transaction: %v", err))
     }
     //Create table for migration
+    tx.Exec(`CREATE TABLE IF NOT EXISTS migration (
+            id integer NOT NULL PRIMARY KEY,
+            version integer NOT NULL,
+            status integer NOT NULL DEFAULT 0,
+            description text)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS migration (
-id integer NOT NULL PRIMARY KEY,
-version integer NOT NULL,
-status integer NOT NULL DEFAULT 0,
-description text)`)
-    tx.Exec(`
-INSERT INTO TABLE migration(version, status, description)
-VALUES(?, ?, ?)
-ON CONFLICT (version)
-DO NOTHING`, m.Version, Succeeded, m.Description)
+        INSERT INTO TABLE migration(version, status, description)
+        VALUES(?, ?, ?)
+        ON CONFLICT (version)
+        DO NOTHING
+        `, m.Version, Succeeded, m.Description)
 
     //Create table for domains
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS clients (
-id integer PRIMARY KEY,
-name text NOT NULL UNIQUE,
-last_seen integer NOT NULL DEFAULT 0,
-ip text)`)
+        CREATE TABLE IF NOT EXISTS clients (
+            id integer PRIMARY KEY,
+            name text NOT NULL UNIQUE,
+            last_seen integer NOT NULL DEFAULT 0,
+            ip text)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS resources (
-id integer PRIMARY KEY,
-name text NOT NULL UNIQUE,
-value text NOT NULL)`)
+        CREATE TABLE IF NOT EXISTS resources (
+            id integer PRIMARY KEY,
+            name text NOT NULL UNIQUE,
+            value text NOT NULL)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS roles (
-id integer PRIMARY KEY,
-name text NOT NULL UNIQUE)`)
+        CREATE TABLE IF NOT EXISTS roles (
+            id integer PRIMARY KEY,
+            name text NOT NULL UNIQUE)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS dhcp (
-id integer PRIMARY KEY,
-first_ip bigint NOT NULL UNIQUE,
-last_ip bigint NOT NULL UNIQUE)`)
+        CREATE TABLE IF NOT EXISTS dhcp (
+            id integer PRIMARY KEY,
+            first_ip bigint NOT NULL UNIQUE,
+            last_ip bigint NOT NULL UNIQUE)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS clients_roles (
-id integer PRIMARY KEY,
-client_id integer NOT NULL,
-role_id integer NOT NULL,
-CONSTRAINT fk_client FOREIGN KEY (client_id)
-REFERENCES clients(id) ON DELETE CASCADE,
-CONSTRAINT fk_role FOREIGN KEY (role_id)
-REFERENCES roles(id) ON DELETE CASCADE)`)
+        CREATE TABLE IF NOT EXISTS clients_roles (
+            id integer PRIMARY KEY,
+            client_id integer NOT NULL,
+            role_id integer NOT NULL,
+            CONSTRAINT fk_client FOREIGN KEY (client_id)
+            REFERENCES clients(id) ON DELETE CASCADE,
+            CONSTRAINT fk_role FOREIGN KEY (role_id)
+            REFERENCES roles(id) ON DELETE CASCADE)`)
     tx.Exec(`
-CREATE TABLE IF NOT EXISTS roles_resources (
-id integer PRIMARY KEY,
-role_id integer NOT NULL,
-resource_id integer NOT NULL,
-CONSTRAINT fk_role FOREIGN KEY (role_id)
-REFERENCES roles(id) ON DELETE CASCADE,
-CONSTRAINT fk_resource FOREIGN KEY (resource_id)
-REFERENCES resources(id) ON DELETE CASCADE)`)
+        CREATE TABLE IF NOT EXISTS roles_resources (
+            id integer PRIMARY KEY,
+            role_id integer NOT NULL,
+            resource_id integer NOT NULL,
+            CONSTRAINT fk_role FOREIGN KEY (role_id)
+            REFERENCES roles(id) ON DELETE CASCADE,
+            CONSTRAINT fk_resource FOREIGN KEY (resource_id)
+            REFERENCES resources(id) ON DELETE CASCADE)`)
     er := tx.Commit()
     if er != nil {
         logger.Debug(fmt.Sprintf("cannot commit transaction: %v", er))
