dfdcc88c797d924496e0a87ba62c9afafd335247
Trieu Truong <quangtrieu1312@gmail.com>
2026-04-08 01:37:24 -0400

    Test

--- server/src/main.go
+++ server/src/main.go
@@ -286,7 +286,7 @@ func run(ctxt context.Context, upChan chan<- bool, bindTo netip.AddrPort, ipProt
 			return nil
 		},
 	}
-	pc, err := lc.ListenPacket(context.Background(), "udp", bindTo.Port().String())
+	pc, err := lc.ListenPacket(context.Background(), "udp", string(bindTo.Port()))
 	if err != nil {
 		return fmt.Errorf("Failed to listen on UDP: %w", err)
 	}
