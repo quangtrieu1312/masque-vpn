05a132b814b0b54861f6ffe3046e912b74e341fd
Trieu Truong <quangtrieu1312@gmail.com>
2026-05-11 08:57:54 -0400

    Improve client build.sh

--- client/build.sh
+++ client/build.sh
@@ -2,10 +2,10 @@ scriptFolder=$(dirname $(realpath $0))
 pushd . >/dev/null 2>&1
 trap 'popd >/dev/null 2>&1' EXIT SIGINT SIGHUP
 cd $scriptFolder
-mkdir -p ./build
-rm -rf ./build/*
+mkdir -p $scriptFolder/build
+rm -rf $scriptFolder/build/*
 cd src
-go build -o ../build/masque
+go build -o $scriptFolder/build/masque
 if [[ $? -ne 0 ]]; then
     echo "Build failed. Aborting."
     exit 1
@@ -13,7 +13,7 @@ fi
 echo "Masque binary is available at $scriptFolder/build/masque"
 mkdir -p /etc/masque/certs
 if [ -f /etc/masque/masque.conf ]; then
-    cp ./masque.conf.template /etc/masque/
+    cp $scriptFolder/masque.conf.template /etc/masque/
     echo "Cannot find /etc/masque/masque.conf. Please create one from masque.conf.template."
 fi
 popd
