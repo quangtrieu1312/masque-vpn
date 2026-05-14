81e5fc3eca5e6e7f50500d993d03e9238ebd31bd
Trieu Truong <quangtrieu1312@gmail.com>
2026-05-14 08:34:12 -0400

    Small fix

--- server/build.sh
+++ server/build.sh
@@ -1,11 +1,4 @@
 pushd . >/dev/null 2>&1
-# Clear stale quic-go cache
-go env GOPATH | xargs -I{} rm -rf {}/pkg/mod/cache/download/github.com/quangtrieu1312/quic-go/@v/v0.59.0*
-go env GOPATH | xargs -I{} rm -rf {}/pkg/mod/github.com/quangtrieu1312/quic-go@v0.59.0
-
-# Clear stale connect-ip-go cache
-go env GOPATH | xargs -I{} rm -rf {}/pkg/mod/cache/download/github.com/quangtrieu1312/connect-ip-go/@v/v0.1.0*
-go env GOPATH | xargs -I{} rm -rf {}/pkg/mod/github.com/quangtrieu1312/connect-ip-go@v0.1.0
 scriptDir=$(dirname $(realpath "$0"))
 mkdir -p $scriptDir/build
 rm -rf $scriptDir/build/*
