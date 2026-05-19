76fa129705d5d03b7bd485491c22360c116eefb0
Trieu Truong <quangtrieu1312@gmail.com>
2026-05-18 20:57:05 -0400

    Try XDP and AF_XDP

--- server/build.sh
+++ server/build.sh
@@ -2,13 +2,12 @@ pushd . >/dev/null 2>&1
 scriptDir=$(dirname $(realpath "$0"))
 mkdir -p $scriptDir/build
 rm -rf $scriptDir/build/*
-cd $scriptDir/src
 if [ "$(uname -m)" = "x86_64" ]; then
     export CGO_CXXFLAGS="-std=c++17 -mcx16"
 else
     export CGO_CXXFLAGS="-std=c++17"
 fi
-go generate -v -x ./xdp/...
-go build -v -x -o $scriptDir/build/bin
+cd $scriptDir/src && go generate ./xdp/...
+go build -o $scriptDir/build/bin
 chmod +x $scriptDir/build/bin
-popd >/dev/null 2>&1
+popd >/dev/null 2>&1 || true
